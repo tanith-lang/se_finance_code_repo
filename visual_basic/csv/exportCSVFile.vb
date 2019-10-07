c2406d6444ba3e14306d7c45de2cb1c25fdbf49d
saurian-dash <saurian_dash@runbox.com>
2019-10-07 17:12:45 +0100

    Another minor update to CSV export VBA

--- visual_basic/csv/exportCSVFile.vb
+++ visual_basic/csv/exportCSVFile.vb
@@ -1,6 +1,87 @@
-Function exportCSVFile(sheetName)
+Option Explicit
+    Dim FSO As Object
+    Dim fName As String
+    Dim fPath As String
+    Dim fYear As String
+    Dim tabName As String
+    Dim testRange As Range
 
-   ' Exports specified sheet name as a .csv file with all values double-quoted
+
+Sub refreshQueries()
+
+    ActiveWorkbook.RefreshAll
+    
+End Sub
+
+
+Sub exportCSV()
+
+'    Assign module level variables
+    fName = "historical__component_booking_base"
+    fYear = sht_controls.Range("F12").Value
+
+    Application.ScreenUpdating = False
+    On Error GoTo errHandler
+
+    Set FSO = CreateObject("Scripting.FileSystemObject")
+    fPath = sht_controls.Range("F20").Value & "/"
+
+    If Not FSO.FolderExists(fPath) Then
+        FSO.CreateFolder (fPath)
+    End If
+    
+    Application.DisplayAlerts = False
+
+    Call ExportCSVFile(sht_01)
+    Call ExportCSVFile(sht_02)
+    Call ExportCSVFile(sht_03)
+    Call ExportCSVFile(sht_04)
+    Call ExportCSVFile(sht_05)
+    Call ExportCSVFile(sht_06)
+    Call ExportCSVFile(sht_07)
+    Call ExportCSVFile(sht_08)
+    Call ExportCSVFile(sht_09)
+    Call ExportCSVFile(sht_10)
+    Call ExportCSVFile(sht_11)
+    Call ExportCSVFile(sht_12)
+
+    Application.ScreenUpdating = True
+    Application.DisplayAlerts = True
+
+    MsgBox "CSV Export Complete"
+
+Exit Sub
+
+errHandler:
+
+    Application.ScreenUpdating = True
+    MsgBox "Error: " & Err.Number & " " & Err.Description
+
+End Sub
+
+Function exportFile(sheetName)
+
+    Dim dateStart As String
+    Dim dateEnd As String
+
+    sheetName.Activate
+    tabName = ActiveSheet.Name
+    dateStart = Format(fYear & "-" & tabName, "yyyy-mm-dd")
+    dateEnd = Format(CDate(WorksheetFunction.EoMonth(dateStart, 0)), "yyyy-mm-dd")
+    
+    Range("A1").CurrentRegion.Copy
+    Workbooks.Add
+    Range("A1").PasteSpecial (xlPasteValuesAndNumberFormats)
+    ActiveWorkbook.SaveAs Filename:=fPath & fName & " " & dateStart & "_" & dateEnd & ".csv", _
+    FileFormat:=xlCSV, CreateBackup:=False, Local:=True
+    
+    Application.CutCopyMode = False
+    ActiveWorkbook.Close
+
+End Function
+
+
+Function ExportCSVFile(sheetName)
 
     Dim DestFile As String
     Dim FileNum As Integer
@@ -10,6 +91,8 @@ Function exportCSVFile(sheetName)
     
     Dim dateStart As String
     Dim dateEnd As String
+    Dim stringStart As String
+    Dim stringEnd As String
 
     sheetName.Activate
     ActiveSheet.ListObjects(1).Range.Select
@@ -17,10 +100,12 @@ Function exportCSVFile(sheetName)
     
     dateStart = Format(fYear & "-" & tabName, "yyyy-mm-dd")
     dateEnd = Format(CDate(WorksheetFunction.EoMonth(dateStart, 0)), "yyyy-mm-dd")
+    stringStart = Format(dateStart, "yyyymmdd")
+    stringEnd = Format(dateEnd, "yyyymmdd")
     
     ' Set source (Target) and destination (DestFile)
     Set Target = Range("A1").CurrentRegion
-    DestFile = fPath & fName & " " & dateStart & "_" & dateEnd & ".csv"
+    DestFile = fPath & fName & " " & stringStart & "_" & stringEnd & ".csv"
     
     ' Obtain next free file handle number.
     FileNum = FreeFile()
